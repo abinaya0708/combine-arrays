e059eab6cb838d831e15773ff9f40b56442564e8
abinaya0708 <aabinaya2001.com>
2023-11-06 12:04:16 +0530

    array

---
+++ array.js
@@ -0,0 +1,19 @@
+document.write("Combine two arrays"+"<br>");
+document.write("Given arrays:"+"<br>"+"a=[1,2,3,4,18,9,34,-2],"+"<br>"+"b=[7,8,9.-7,3]"+"<br>");
+
+//numbers
+var a=[1,2,3,4,18,9,34,-2];
+var b=[7,8,9,-7,3];
+
+
+var result= a.push(b);
+document.write("output:"+a+"<br>");
+
+//String
+document.write("Given arrays:"+"a=['hi','hello'],"+"<br>"+"b=['welcome']"+"<br>");
+
+var a=["hi","hello"];
+var b=["welcome"];
+
+var result= a.push(b);
+document.write("output:"+a);
